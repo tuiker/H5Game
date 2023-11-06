271586540037484e9790c322c2dfa3166098edd6
GaoLu <349918386@qq.com>
2023-11-06 10:42:24 +0800

    更改表结构

---
+++ src/main/resources/sql/231106Gl.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `h5_game`.`game_review`
+    MODIFY COLUMN `review_content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '评论内容' AFTER `game_id`,
+    MODIFY COLUMN `review_time` datetime NOT NULL COMMENT '评论时间' AFTER `review_content`,
+    ADD COLUMN `review_grade` int NULL COMMENT '评论打分' AFTER `help_num`,
+    ADD COLUMN `reply_content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '回复内容' AFTER `review_grade`,
+    ADD COLUMN `reply_time` datetime(0) NULL COMMENT '回复时间' AFTER `reply_content`,
+    ADD COLUMN `have_reply` int NOT NULL DEFAULT 0 COMMENT '是否回复：0否；1是' AFTER `reply_time`;
