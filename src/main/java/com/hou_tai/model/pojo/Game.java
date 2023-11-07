04674ed111af0731414c35ee87dde0325f787994
GaoLu <349918386@qq.com>
2023-11-07 16:24:29 +0800

    游戏，游戏评论日期格式问题

--- src/main/java/com/hou_tai/model/pojo/Game.java
+++ src/main/java/com/hou_tai/model/pojo/Game.java
@@ -1,15 +1,13 @@
 package com.hou_tai.model.pojo;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.baomidou.mybatisplus.annotation.*;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.hou_tai.model.base.BaseEntity;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Data
 @Builder
@@ -22,80 +20,116 @@ public class Game implements Serializable {
 
     @Schema(title = "游戏ID")
     @TableId(value = "id", type = IdType.AUTO)
-    private long id ;
-    /** 游戏名称 */
+    private long id;
+    /**
+     * 游戏名称
+     */
     @Schema(title = "游戏名称")
     @TableField(value = "game_name")
-    private String gameName ;
-    /** 游戏类型 */
+    private String gameName;
+    /**
+     * 游戏类型
+     */
     @Schema(title = "游戏类型")
     @TableField(value = "game_type")
-    private Integer gameType ;
-    /** 游戏语言ID */
+    private Integer gameType;
+    /**
+     * 游戏语言ID
+     */
     @Schema(title = "游戏语言ID")
     @TableField(value = "language_id")
-    private Integer languageId ;
-    /** 游戏LOGO */
+    private Integer languageId;
+    /**
+     * 游戏LOGO
+     */
     @Schema(title = "游戏LOGO")
     @TableField(value = "game_logo")
-    private String gameLogo ;
-    /** 游戏主图 */
+    private String gameLogo;
+    /**
+     * 游戏主图
+     */
     @Schema(title = "游戏主图")
     @TableField(value = "game_main_logo")
-    private String gameMainLogo ;
-    /** 游戏背景 */
+    private String gameMainLogo;
+    /**
+     * 游戏背景
+     */
     @Schema(title = "游戏背景")
     @TableField(value = "game_background")
-    private String gameBackground ;
-    /** 游戏地址 */
+    private String gameBackground;
+    /**
+     * 游戏地址
+     */
     @Schema(title = "游戏地址")
     @TableField(value = "game_url")
-    private String gameUrl ;
-    /** 游戏描述 */
+    private String gameUrl;
+    /**
+     * 游戏描述
+     */
     @Schema(title = "游戏描述")
     @TableField(value = "game_desc")
-    private String gameDesc ;
-    /** 数据安全 */
+    private String gameDesc;
+    /**
+     * 数据安全
+     */
     @Schema(title = "数据安全")
     @TableField(value = "data_security")
-    private String dataSecurity ;
-    /** 游戏评分 */
+    private String dataSecurity;
+    /**
+     * 游戏评分
+     */
     @Schema(title = "游戏评分")
     @TableField(value = "game_grade")
-    private Integer gameGrade ;
-    /** 游戏下载次数 */
+    private Integer gameGrade;
+    /**
+     * 游戏下载次数
+     */
     @Schema(title = "游戏下载次数")
     @TableField(value = "game_download")
-    private Integer gameDownload ;
-    /** 游戏适合年龄 */
+    private Integer gameDownload;
+    /**
+     * 游戏适合年龄
+     */
     @Schema(title = "游戏适合年龄")
     @TableField(value = "game_age")
-    private Integer gameAge ;
-    /** 开发者邮箱 */
+    private Integer gameAge;
+    /**
+     * 开发者邮箱
+     */
     @Schema(title = "开发者邮箱")
     @TableField(value = "dev_email")
-    private String devEmail ;
-    /** 开发者地址 */
+    private String devEmail;
+    /**
+     * 开发者地址
+     */
     @Schema(title = "开发者地址")
     @TableField(value = "dev_url")
-    private String devUrl ;
-    /** 创建用户ID */
+    private String devUrl;
+    /**
+     * 创建用户ID
+     */
     @Schema(title = "创建用户ID")
     @TableField(value = "create_id")
-    private Long createId ;
-    /**  */
-    @TableField(value = "create_time",fill = FieldFill.INSERT) // 插入自动填充
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Long createId;
+    /**
+     *
+     */
+    @TableField(value = "create_time", fill = FieldFill.INSERT) // 插入自动填充
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
-    /** 更新用户ID */
+    /**
+     * 更新用户ID
+     */
     @Schema(title = "更新用户ID")
     @TableField(value = "update_id")
-    private Long updateId ;
-    /**  */
+    private Long updateId;
+    /**
+     *
+     */
     @Schema(title = "游戏更新日期")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")  //前后端接收日期都可以改变格式
+    @JSONField(format = "yyyy-MM-dd HH:mm:ss") //ResponseBody做了拦截，封装了一层导致原来JsonFormat无效，前端返回日期，要用JSONField来处理
     @TableField(value = "update_time")
     private LocalDateTime updateTime;
 
@@ -105,7 +139,7 @@ public class Game implements Serializable {
 
     @Schema(title = "游戏评论数")
     @TableField(value = "review_num")
-    private Integer reviewNum ;
+    private Integer reviewNum;
 
 
 }
--- src/main/java/com/hou_tai/model/pojo/ReviewReply.java
+++ src/main/java/com/hou_tai/model/pojo/ReviewReply.java
@@ -1,12 +1,12 @@
 package com.hou_tai.model.pojo;
 
+import com.alibaba.fastjson.annotation.JSONField;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -20,20 +20,28 @@ import java.util.Date;
 public class ReviewReply {
 
     @TableId(value = "id", type = IdType.AUTO)
-    private Integer id ;
-    /** 用户ID */
+    private Integer id;
+    /**
+     * 用户ID
+     */
     @Schema(title = "用户ID")
-    private Long userId ;
-    /** 评论ID */
+    private Long userId;
+    /**
+     * 评论ID
+     */
     @Schema(title = "评论ID")
-    private Integer reviewId ;
-    /** 回复内容 */
+    private Integer reviewId;
+    /**
+     * 回复内容
+     */
     @Schema(title = "回复内容")
-    private String replyContent ;
-    /** 回复时间 */
+    private String replyContent;
+    /**
+     * 回复时间
+     */
     @Schema(title = "回复时间")
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JSONField(format = "yyyy-MM-dd HH:mm:ss") //ResponseBody做了拦截，封装了一层导致原来JsonFormat无效，前端返回日期，要用JSONField来处理
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private Date replyTime ;
+    private Date replyTime;
 
 }
