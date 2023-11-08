a50166ef82bc8180cd74e2ebda664fdb6751f894
GaoLu <349918386@qq.com>
2023-11-08 19:23:16 +0800

    数据概括接口实现

--- src/main/java/com/hou_tai/controller/pc/DataOverviewController.java
+++ src/main/java/com/hou_tai/controller/pc/DataOverviewController.java
@@ -1,21 +1,15 @@
 package com.hou_tai.controller.pc;
 
 
-import cn.hutool.core.util.ObjectUtil;
-import com.hou_tai.model.dto.DataDto;
 import com.hou_tai.response.ResponseData;
-import com.hou_tai.response_vo.DataBoardVo;
 import com.hou_tai.response.ResultVO;
 import com.hou_tai.service.IDataOverviewService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Objects;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
@@ -30,14 +24,15 @@ public class DataOverviewController {
 
     @Resource
     IDataOverviewService dataOverviewService;
-    @Operation(summary = "数据概览",description = "1看板数据2折线数据3概览数据")
+
+    /**
+     * @Author GaoLu
+     * @Date 2023/11/8
+     **/
+    @Operation(summary = "数据概括", description = "数据概括")
     @PostMapping("/getAllStates")
-    @ApiResponse(responseCode = "200", description = "查找成功")
-    public ResultVO getAllStates(@RequestBody DataDto dto){
-        if(ObjectUtil.isEmpty(dto)||dto.getStartDate()==null){//默认7天
-            dto=DataDto.builder().startDate(LocalDate.now().minusDays(6l)).endDate(LocalDate.now()).build();
-        }
-        return ResponseData.success(dataOverviewService.getAllStates(dto));
+    public ResultVO getAllStates() {
+        return ResponseData.success(dataOverviewService.getAllStates());
     }
 
 
--- src/main/java/com/hou_tai/model/dao/ChannelDataMapper.java
+++ src/main/java/com/hou_tai/model/dao/ChannelDataMapper.java
@@ -1,8 +1,27 @@
 package com.hou_tai.model.dao;
 
 import com.hou_tai.model.pojo.ChannelData;
+import com.hou_tai.response_vo.DataOfTimeVo;
+
+import java.util.List;
 
 public interface ChannelDataMapper {
+    /**
+     * @Description 获取今天请求次数
+     * @Author GaoLu
+     * @Date 2023/11/8
+     * @Return 总数
+     **/
+    Integer getCountByToday(long gameId);
+
+    /**
+     * @Description 7日内时间数据
+     * @Author GaoLu
+     * @Date  2023/11/8
+     * @Return
+    **/
+    List<DataOfTimeVo> getNumForSevenDay();
+
     int deleteByPrimaryKey(Integer id);
 
     int insert(ChannelData record);
