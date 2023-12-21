1f3397cbec15fea5cdc9d3b2352765aac7c1a9d3
Yanxiaofei0916 <591482927@qq.com>
2023-12-21 15:42:20 +0800

    1、新增编辑游戏时，校验游戏名称和APK包名是否已存在，已存在则给予提示不予保存

--- src/main/java/com/hou_tai/service/IGameService.java
+++ src/main/java/com/hou_tai/service/IGameService.java
@@ -1,6 +1,7 @@
 package com.hou_tai.service;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hou_tai.common.response.ResultVO;
 import com.hou_tai.controller.mobile.dto.MobileGameDto;
 import com.hou_tai.controller.mobile.dto.MobileHomeGameDto;
 import com.hou_tai.controller.pc.dto.GameAddReqDTO;
@@ -51,14 +52,14 @@ public interface IGameService {
      * @param gameAddReqDTO 实例对象
      * @return 实例对象
      */
-    Game insert(GameAddReqDTO gameAddReqDTO);
+    ResultVO<Boolean> insert(GameAddReqDTO gameAddReqDTO);
     /**
      * 更新数据
      *
      * @param reqDTO 实例对象
      * @return 实例对象
      */
-    Boolean update(GameUpdateReqDTO reqDTO);
+    ResultVO<Boolean> update(GameUpdateReqDTO reqDTO);
     /**
      * 通过主键删除数据
      *
